c81873014513cd7cd56add62315b6cf552b0c585
Greg Chance <ge.chance@gmail.com>
2020-05-12 14:04:49 -0700

    twosum

---
+++ leetcode.js
@@ -0,0 +1,17 @@
+
+
+// var twoSum = function(nums, target) {
+//     for(let i=0;i<nums.length-1;i++) {
+//         for(let j=i;j<nums.length-1;j++) {
+//             if(nums[j] > target) {
+//                 break;
+//             } else if(nums[j] + nums[j+1] == target) {
+//                 console.log(nums[j])
+//                 return (j)+","+(j+1);
+//             }
+//         }
+//     }
+//     return false;
+// };
+
+// console.log(twoSum([2,7,11,15], 9))
